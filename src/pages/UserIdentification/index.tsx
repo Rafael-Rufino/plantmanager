1a14adba29d781dcb2910a01d466f4afe5d7b414
RafaelRufino <rafaelrufino.52@gmail.com>
2022-06-30 19:33:14 -0300

    feat: criando a tela de identificar usuario

---
+++ src/pages/UserIdentification/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/core";
+
+import {
+  Text,
+  SafeAreaView,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+import colors from "../../../styles/colors";
+import { Button } from "../../components/Button";
+
+import { styles } from "./styles";
+
+export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>("");
+
+  const navigation = useNavigation();
+
+  function handleSubmit() {
+    navigation.navigate("Confirmation");
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  }
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.emoji}>{isFilled ? "😃" : "😐"}</Text>
+              <Text style={styles.title}>Como podemos{"\n"} chamar você?</Text>
+            </View>
+
+            <TextInput
+              style={[
+                styles.input,
+                (isFocused || isFilled) && { borderColor: colors.green },
+              ]}
+              placeholder="Digite um nome"
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
+            />
+            <View style={styles.footer}>
+              <Button title="Confirmar" onPress={handleSubmit} />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
