ddc27d523c7a7ef2bd40e3672a20af59bfc66761
RafaelRufino <rafaelrufino.52@gmail.com>
2022-06-30 19:40:24 -0300

    refactor: utilizando usabilidade para fecha teclad

--- src/pages/UserIdentification/index.tsx
+++ src/pages/UserIdentification/index.tsx
@@ -7,7 +7,9 @@ import {
   View,
   TextInput,
   KeyboardAvoidingView,
+  TouchableWithoutFeedback,
   Platform,
+  Keyboard,
 } from "react-native";
 
 import colors from "../../../styles/colors";
@@ -44,28 +46,33 @@ export function UserIdentification() {
         style={styles.container}
         behavior={Platform.OS == "ios" ? "padding" : "height"}
       >
-        <View style={styles.content}>
-          <View style={styles.form}>
-            <View style={styles.header}>
-              <Text style={styles.emoji}>{isFilled ? "😃" : "😐"}</Text>
-              <Text style={styles.title}>Como podemos{"\n"} chamar você?</Text>
-            </View>
+        {/* TouchableWithoutFeedback  - usabilidade por voltar  da tela quando o teclado aparece */}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>{isFilled ? "😃" : "😐"}</Text>
+                <Text style={styles.title}>
+                  Como podemos{"\n"} chamar você?
+                </Text>
+              </View>
 
-            <TextInput
-              style={[
-                styles.input,
-                (isFocused || isFilled) && { borderColor: colors.green },
-              ]}
-              placeholder="Digite um nome"
-              onBlur={handleInputBlur}
-              onFocus={handleInputFocus}
-              onChangeText={handleInputChange}
-            />
-            <View style={styles.footer}>
-              <Button title="Confirmar" onPress={handleSubmit} />
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green },
+                ]}
+                placeholder="Digite um nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+              <View style={styles.footer}>
+                <Button title="Confirmar" onPress={handleSubmit} />
+              </View>
             </View>
           </View>
-        </View>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
