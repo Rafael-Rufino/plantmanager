4fbda69b9b57d103725c1b106973fef304c4b895
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-05 00:44:47 -0300

    feat: incluindo interface de plantas agendadas

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,20 +1,43 @@
+import { useNavigation, useRoute } from "@react-navigation/core";
+
 import React from "react";
 import { View, SafeAreaView, Text } from "react-native";
 import { Button } from "../../components/Button";
 
 import { styles } from "./styles";
 
+interface IParams {
+  title: string;
+  subTitle: string;
+  buttonTitle: string;
+  icon: "smile" | "hub";
+  nextScreen: string;
+}
+
+const emojis = {
+  smile: "😀",
+  hub: "💡",
+};
+
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  const route = useRoute();
+
+  const { title, subTitle, buttonTitle, icon, nextScreen } =
+    route.params as IParams;
+
+  function handlePLantSelect() {
+    navigation.navigate(nextScreen);
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.emoji}>😀</Text>
-        <Text style={styles.title}>Prontinho</Text>
-        <Text style={styles.subTitle}>
-          Agora vamos começar a cuidar das suas plantinhas com muito cuidado
-        </Text>
+        <Text style={styles.emoji}>{emojis[icon]}</Text>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
         <View style={styles.footer}>
-          <Button title="Confirmar" />
+          <Button title={buttonTitle} onPress={handlePLantSelect} />
         </View>
       </View>
     </SafeAreaView>
